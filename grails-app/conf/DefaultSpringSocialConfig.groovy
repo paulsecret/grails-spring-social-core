b1a0dddbd62ffd5408f4c016755a97c193eb0f53
Domingo Suarez Torres <domingo.suarez@gmail.com>
2011-08-05 17:56:45 -0500

    removed twiiter and facebook related config

--- grails-app/conf/DefaultSpringSocialConfig.groovy
+++ grails-app/conf/DefaultSpringSocialConfig.groovy
@@ -14,14 +14,4 @@
  * limitations under the License.
  */
 springsocial {
-    twitter.page.connect = "/springsocial/twitter/connect"
-    twitter.page.connectedHome = "/springSocialTwitter/index"
-    twitter.page.profile = "/springSocialTwitter/index"
-    twitter.page.timeLine = "/springsocial/twitter/timeline"
-    twitter.page.profiles = "/springsocial/twitter/profiles"
-    twitter.page.directMessages = "/springsocial/twitter/messages"
-    twitter.page.trends = "/springsocial/twitter/trends"
-
-    facebook.page.connect = "/springsocial/facebook/connect"
-    facebook.page.connectedHome = "/springsocial/facebook/index"
 }
