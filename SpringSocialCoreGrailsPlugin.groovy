f9f5d636f4ad17eee913891d5ee8febb2d838451
Domingo Suarez Torres <domingo.suarez@gmail.com>
2011-09-10 22:37:40 -0500

    polish code

--- SpringSocialCoreGrailsPlugin.groovy
+++ SpringSocialCoreGrailsPlugin.groovy
@@ -16,58 +16,58 @@
 import grails.plugins.springsocial.SpringSecuritySigninService
 
 class SpringSocialCoreGrailsPlugin {
-    // the plugin version
-    def version = "0.1.5"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [pluginConfig: '0.1.4 > *', springSecurityCore: '1.2 > *']
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp",
-            "grails-app/conf/Config.groovy",
-            "grails-app/conf/DataSource.groovy",
-            "grails-app/i18n/**"
-    ]
+  // the plugin version
+  def version = "0.1.5"
+  // the version or versions of Grails the plugin is designed for
+  def grailsVersion = "1.3 > *"
+  // the other plugins this plugin depends on
+  def dependsOn = [pluginConfig: '0.1.4 > *', springSecurityCore: '1.2 > *']
+  // resources that are excluded from plugin packaging
+  def pluginExcludes = [
+      "grails-app/views/error.gsp",
+      "grails-app/conf/Config.groovy",
+      "grails-app/conf/DataSource.groovy",
+      "grails-app/i18n/**"
+  ]
 
-    // TODO Fill in these fields
-    def author = "Domingo Suarez Torres"
-    def authorEmail = "domingo.suarez@gmail.com"
-    def title = "Spring Social Core"
-    def description = '''\\
+  // TODO Fill in these fields
+  def author = "Domingo Suarez Torres"
+  def authorEmail = "domingo.suarez@gmail.com"
+  def title = "Spring Social Core"
+  def description = '''\\
 Spring Social Core plugin.
 '''
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/spring-social-core"
+  // URL to the plugin's documentation
+  def documentation = "http://grails.org/plugin/spring-social-core"
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
-    }
+  def doWithWebDescriptor = { xml ->
+    // TODO Implement additions to web.xml (optional), this event occurs before
+  }
 
-    def doWithSpring = {
-        xmlns context: "http://www.springframework.org/schema/context"
-        context.'component-scan'('base-package': "grails.plugins.springsocial.config.core")
+  def doWithSpring = {
+    xmlns context: "http://www.springframework.org/schema/context"
+    context.'component-scan'('base-package': "grails.plugins.springsocial.config.core")
 
-        signInService(SpringSecuritySigninService)
-    }
+    signInService(SpringSecuritySigninService)
+  }
 
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
+  def doWithDynamicMethods = { ctx ->
+    // TODO Implement registering dynamic methods to classes (optional)
+  }
 
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
+  def doWithApplicationContext = { applicationContext ->
+    // TODO Implement post initialization spring config (optional)
+  }
 
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
+  def onChange = { event ->
+    // TODO Implement code that is executed when any artefact that this plugin is
+    // watching is modified and reloaded. The event contains: event.source,
+    // event.application, event.manager, event.ctx, and event.plugin.
+  }
 
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
+  def onConfigChange = { event ->
+    // TODO Implement code that is executed when the project configuration changes.
+    // The event is the same as for 'onChange'.
+  }
 }
