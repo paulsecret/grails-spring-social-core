ce3206de2af7ede16d1ff795b55e8f258600d510
Domingo Suarez Torres <domingo.suarez@gmail.com>
2011-09-10 22:33:06 -0500

    polish code

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,37 +17,37 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.dependency.resolution = {
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
+  inherits("global") {
+    // uncomment to disable ehcache
+    // excludes 'ehcache'
+  }
+  log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+  repositories {
+    grailsPlugins()
+    grailsHome()
+    grailsCentral()
 
-        // from public Maven repositories
-        mavenLocal()
-        mavenCentral()
+    // from public Maven repositories
+    mavenLocal()
+    mavenCentral()
 
-        mavenRepo "http://maven.springframework.org/release"
-        mavenRepo "http://maven.springframework.org/snapshot"
-        mavenRepo "http://maven.springframework.org/milestone"
-    }
-    plugins {
-		compile ":plugin-config:0.1.4"
-        compile ":spring-security-core:1.2"
-    }
-    dependencies {
-        def springSocialVersion = "1.0.0.RELEASE"
+    mavenRepo "http://maven.springframework.org/release"
+    mavenRepo "http://maven.springframework.org/snapshot"
+    mavenRepo "http://maven.springframework.org/milestone"
+  }
+  plugins {
+    compile ":plugin-config:0.1.4"
+    compile ":spring-security-core:1.2"
+  }
+  dependencies {
+    def springSocialVersion = "1.0.0.RELEASE"
 
-        compile("org.springframework.social:spring-social-core:${springSocialVersion}") { transitive = false }
-        compile("org.springframework.social:spring-social-web:${springSocialVersion}") { transitive = false }
+    compile("org.springframework.social:spring-social-core:${springSocialVersion}") { transitive = false }
+    compile("org.springframework.social:spring-social-web:${springSocialVersion}") { transitive = false }
 
 
-        compile("org.springframework.security:spring-security-crypto:3.1.0.RC3") { transitive = false }
-        compile("javax.inject:javax.inject:1")
-        compile 'org.codehaus.jackson:jackson-mapper-asl:1.8.5'
-    }
+    compile("org.springframework.security:spring-security-crypto:3.1.0.RC3") { transitive = false }
+    compile("javax.inject:javax.inject:1")
+    compile 'org.codehaus.jackson:jackson-mapper-asl:1.8.5'
+  }
 }
