66f7b24f96035029b73dce661ece78b10ca4c47d
Domingo Suarez Torres <domingo.suarez@gmail.com>
2011-10-03 20:57:33 -0500

    preparing 0.1.9 release

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -45,4 +45,7 @@ grails.project.dependency.resolution = {
     compile("javax.inject:javax.inject:1")
     compile 'org.codehaus.jackson:jackson-mapper-asl:1.8.5'
   }
+  plugins {
+    compile(":spring-security-core:1.2.1")
+  }
 }
