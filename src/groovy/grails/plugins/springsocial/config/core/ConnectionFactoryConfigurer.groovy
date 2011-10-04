a355dd513a677094c8f1d7b6c616f60e315e1216
Domingo Suarez Torres <domingo.suarez@gmail.com>
2011-10-04 17:49:49 -0500

    added a ConnectionFactoryConfigurer

---
+++ src/groovy/grails/plugins/springsocial/config/core/ConnectionFactoryConfigurer.groovy
@@ -0,0 +1,42 @@
+/* Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package grails.plugins.springsocial.config.core
+
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
+import org.springframework.beans.factory.support.BeanDefinitionRegistry
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.social.connect.ConnectionFactory
+
+@Configuration
+class ConnectionFactoryConfigurer {
+  @Bean
+  BeanDefinitionRegistryPostProcessor processConecctionF() {
+    new BeanDefinitionRegistryPostProcessor() {
+      void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry beanDefinitionRegistry) {}
+
+      void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) {
+        log.info 'postProcessBeanFactory start'
+
+        //def appConfig = beanFactory.parentBeanFactory.getBean('grailsApplication').config
+
+        def connectionFactories = beanFactory.getBeansOfType(ConnectionFactory)
+        println connectionFactories.dump()
+      }
+
+    }
+  }
+}
