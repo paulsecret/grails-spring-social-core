e1b43fcb0bf875f7046afa5024cf1e67b59ac6b8
Domingo Suarez Torres <domingo.suarez@gmail.com>
2012-04-18 11:49:42 -0500

    When the app is running on Grails 1.3.4, prevent load the transitive plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,17 +1,17 @@
 /* Copyright 2012 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
@@ -20,7 +20,7 @@ grails.project.dependency.resolution = {
     // uncomment to disable ehcache
     // excludes 'ehcache'
   }
-  log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+  log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
   repositories {
     grailsPlugins()
     grailsHome()
@@ -46,11 +46,15 @@ grails.project.dependency.resolution = {
     compile("javax.inject:javax.inject:1")
   }
   plugins {
+    if (grailsVersion.startsWith('2')) {
+      test(":code-coverage:1.2.5") { export = false }
+      test("org.spockframework:spock:0.6-groovy-1.8-SNAPSHOT") { export = false }
+      build(":release:2.0.0") { export = false }
+      build(":rest-client-builder:1.0.2") { export = false }
+    }
+
     compile(":spring-security-core:1.2.7.3")
-    test(":code-coverage:1.2.5") { export = false }
-    test("org.spockframework:spock:0.6-groovy-1.8-SNAPSHOT") { export = false }
-    build(":release:2.0.0") { export = false }
-    build(":rest-client-builder:1.0.2") { export = false }
+
   }
 }
 
