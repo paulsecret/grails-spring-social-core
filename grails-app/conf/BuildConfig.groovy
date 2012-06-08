5584a51e7699d2013c4e25c8ec1ea38c9615e53e
Domingo Suarez Torres <domingo.suarez@gmail.com>
2012-06-08 11:02:40 -0500

    removed spock plugin, closes #7

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -48,23 +48,19 @@ grails.project.dependency.resolution = {
   plugins {
     /*
     This validation is for prevent load the following plugins in previous Grails versions.
-    I some Grails versions from 1.3.* the 'export = false' does not work. For Grails 2.* works properly
+    In some Grails versions from 1.3.* the 'export = false' does not work. For Grails 2.* works properly
     */
     if (grailsVersion.startsWith('2')) {
       compile(":spring-security-core:1.2.7.3")
       test(":code-coverage:1.2.5") { export = false }
-      test("org.spockframework:spock:0.6-groovy-1.8-SNAPSHOT") { export = false }
       build(":release:2.0.0") { export = false }
       build(":rest-client-builder:1.0.2") { export = false }
     }
   }
 }
 
-grails.project.repos.clickonero.url = "http://repo.clickonero.com/nexus/content/repositories/snapshots/"
-
 grails.release.scm.enabled = false
-//grails.project.repos.default = "grailsCentral"
-grails.project.repos.default = "clickonero"
+grails.project.repos.default = "grailsCentral"
 
 coverage {
   exclusions = [
