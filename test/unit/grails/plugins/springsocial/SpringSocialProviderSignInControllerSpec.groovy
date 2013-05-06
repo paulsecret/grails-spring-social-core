b10e6a67340d4d5c0b0bf3fed0329c6453300070
Domingo Suarez Torres <domingo.suarez@gmail.com>
2013-05-06 14:35:54 -0500

    added Spec for SpringSocialProviderSignInController

---
+++ test/unit/grails/plugins/springsocial/SpringSocialProviderSignInControllerSpec.groovy
@@ -0,0 +1,11 @@
+package grails.plugins.springsocial
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: domix
+ * Date: 06/05/13
+ * Time: 14:33
+ * To change this template use File | Settings | File Templates.
+ */
+class SpringSocialProviderSignInControllerSpec {
+}
