6add804099a79e62389e1763d16315973e5ed84e
Domingo Suarez Torres <domingo.suarez@gmail.com>
2013-05-06 22:14:58 -0500

    added some test for SpringSocialProviderSignInController

--- test/unit/grails/plugins/springsocial/SpringSocialProviderSignInControllerSpec.groovy
+++ test/unit/grails/plugins/springsocial/SpringSocialProviderSignInControllerSpec.groovy
@@ -1,4 +1,20 @@
+/* Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package grails.plugins.springsocial
+
+import grails.test.mixin.TestFor
 
 /**
  * Created with IntelliJ IDEA.
@@ -7,5 +23,11 @@ package grails.plugins.springsocial
  * Time: 14:33
  * To change this template use File | Settings | File Templates.
  */
-class SpringSocialProviderSignInControllerSpec {
+@TestFor(SpringSocialProviderSignInController)
+class SpringSocialProviderSignInControllerSpec extends spock.lang.Specification {
+
+  void "ss s"() {
+    controller.s
+  }
+
 }
