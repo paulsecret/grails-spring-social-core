389de13be78377e3618cb24fe3b70f8b0ddb272e
Domingo Suarez Torres <domingo.suarez@gmail.com>
2013-09-29 20:51:18 -0500

    upgraed some plugins and dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -16,6 +16,21 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
+grails.project.fork = [
+    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
+    // configure settings for the test-app JVM, uses the daemon by default
+    //test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    // configure settings for the run-app JVM
+    //run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the run-war JVM
+    //war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the Console UI JVM
+    //console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
+
+grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
   inherits("global") {
     // uncomment to disable ehcache
@@ -35,7 +50,7 @@ grails.project.dependency.resolution = {
     mavenRepo "http://maven.springframework.org/snapshot"
     mavenRepo "http://maven.springframework.org/milestone"
 
-    grailsRepo "http://grails.org/plugins"
+    mavenRepo "http://grails.org/plugins"
   }
   dependencies {
     def springSocialVersion = "1.0.3.RELEASE"
@@ -43,12 +58,11 @@ grails.project.dependency.resolution = {
     compile("org.springframework.social:spring-social-core:${springSocialVersion}") { transitive = false }
     compile("org.springframework.social:spring-social-web:${springSocialVersion}") { transitive = false }
 
-    compile("org.springframework.security:spring-security-crypto:3.1.0.RELEASE") { transitive = false }
+    compile("org.springframework.security:spring-security-crypto:3.1.4.RELEASE") { transitive = false }
     compile("javax.inject:javax.inject:1")
 
     test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
     test "org.gebish:geb-spock:0.9.0"
-    test 'org.mockito:mockito-all:1.9.5'
   }
   plugins {
 
@@ -58,14 +72,14 @@ grails.project.dependency.resolution = {
       export = false
     }
 
-    test(":geb:0.9.1", ":functional-test-development:0.9.4", ":fixtures:1.3-SNAPSHOT", ":codenarc:0.19", ":code-coverage:1.2.6", ":guard:1.0.7") {
+    test(":geb:0.9.2-SNAPSHOT", ":functional-test-development:0.9.4", ":fixtures:1.3-SNAPSHOT", ":codenarc:0.19", ":code-coverage:1.2.6", ":guard:1.0.7") {
       export = false
     }
     test(":spock:0.7") {
       exclude "spock-grails-support"
       export = false
     }
-    build(':release:3.0.0', ':rest-client-builder:1.0.3') {
+    build(':release:3.0.1', ':rest-client-builder:1.0.3') {
       export = false
     }
   }
